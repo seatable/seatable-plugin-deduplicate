d0e3d6db0d0ff6a540c57d6ced73441f722e0813
Michael An <1822852997@qq.com>
2020-09-10 14:14:14 +0800

    change thumbnail fn

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,9 @@
 export const getImageThumbnailUrl = (url) => {
-  let { server, thumbnailServer, accessToken } = window.dtable || {};
-  let isInternalLink = url.indexOf(server) > -1; 
+  let { server } = window.dtable;
+  let isInternalLink = url.indexOf(server) > -1;
   if (isInternalLink) {
-    if (thumbnailServer) {
-      let imageThumbnailUrl = url.replace(server, thumbnailServer);
-      imageThumbnailUrl = imageThumbnailUrl.replace('/workspace', '/thumbnail/workspace') + '?size=256&token=' + accessToken;
-      return imageThumbnailUrl;
-    }   
-    else {
-      let imageThumbnailUrl = url.replace('/workspace', '/thumbnail/workspace') + '?size=256';
-      return imageThumbnailUrl;
-    }   
+    let imageThumbnailUrl = url.replace('/workspace', '/thumbnail/workspace') + '?size=256';
+    return imageThumbnailUrl;
   }
   return url;
 };
