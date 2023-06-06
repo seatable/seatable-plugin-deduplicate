5a998417eb527064254f20ee92d05bc82bd1fe73
Michael An <2331806369@qq.com>
2023-06-06 11:16:45 +0800

    change translation

--- src/locale/lang/fr.js
+++ src/locale/lang/fr.js
@@ -15,10 +15,10 @@ const fr = {
   "Select": "Sélectionner",
   "Cancel": "Annuler",
   "This_plugin_is_not_available_now": "Le plugin n'est pas disponible pour vous. Vous avez un accès en lecture seule.",
-  "Delete_all_duplicated_items": "Delete all the duplicated records",
-  "keep_only_one": ", only keep the first one or the last one.",
-  "keep_first": "Keep the first one",
-  "keep_last": "Keep the last one"
+  "Delete_all_duplicated_items": "Supprimer tous les doublons",
+  "keep_only_one": ", garder le premier ou le dernier.",
+  "keep_first": "Garder le premier",
+  "keep_last": "Garder le dernier"
 }
 ;
 
