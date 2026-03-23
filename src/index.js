0c576efc415f57b0f7d0b551b7d3d15b2be5a816
张玉鑫 <zhangyuxin9102@163.com>
2026-03-23 14:34:43 +0800

    Update dtable-sdk to 5.0.10 and improve local development initialization

--- src/index.js
+++ src/index.js
@@ -12,19 +12,24 @@ class TaskList {
     // local develop
     window.app = {};
     window.app.state = {};
-    window.dtable  = {};
+    window.dtable = {
+      ...window.dtablePluginConfig,
+    };
     await dtableSDK.init(window.dtablePluginConfig);
     await dtableSDK.syncWithServer();
+    await dtableSDK.dtableWebAPI.login();
 
-    window.dtableWebAPI = dtableSDK.dtableWebAPI;
     window.app.collaborators = dtableSDK.dtableStore.collaborators;
+    window.app.collaboratorsCache = {};
     window.app.state.collaborators = dtableSDK.dtableStore.collaborators;
-    window.dtable.mediaUrl = window.dtablePluginConfig.mediaUrl;
+    window.dtableWebAPI = dtableSDK.dtableWebAPI;
+    window.app.onClosePlugin = () => { };
     window.dtableSDK = dtableSDK;
+    window.dtable = Object.assign(window.dtable, dtableSDK.config);
   }
 
   static async execute() {
-    await this.init()
+    await this.init();
     const root = createRoot(document.getElementById('root'));
     root.render(<App isDevelopment showDialog />);
   }
